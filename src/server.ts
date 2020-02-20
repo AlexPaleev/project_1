0be2a6048a08b822385ac690706bb7e07ed6a75c
Alexander Paleev <alexandr.pavleev@tech-stack.io>
2020-02-20 10:19:20 +0200

    add controllers in server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import App from './app';
 import config from './ormconfig';
 import UsersController from './routes/user.rout';
 import ProjectController from './routes/project.rout';
+import Dev_projectController from 'routes/dev_project.rout';
+import Users_roleController from 'routes/user_role.rout';
+import RoleController from 'routes/role.rout';
  
 (async () => {
   try {
@@ -15,7 +18,11 @@ import ProjectController from './routes/project.rout';
   }
   const app = new App(
     [
-      new UsersController, new ProjectController
+      new UsersController, 
+      new ProjectController,
+      new Dev_projectController,
+      new Users_roleController,
+      new RoleController
     ],
     5000
   );
