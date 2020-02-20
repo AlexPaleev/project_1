cdb4575655245b921966f5d6cc533b753e469c97
Alexander Paleev <alexandr.pavleev@tech-stack.io>
2020-02-20 10:24:52 +0200

    fixed errors

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import App from './app';
 import config from './ormconfig';
 import UsersController from './routes/user.rout';
 import ProjectController from './routes/project.rout';
-import Dev_projectController from 'routes/dev_project.rout';
-import Users_roleController from 'routes/user_role.rout';
-import RoleController from 'routes/role.rout';
+import Dev_projectController from './routes/dev_project.rout';
+import Users_roleController from './routes/user_role.rout';
+import RoleController from './routes/role.rout';
  
 (async () => {
   try {
